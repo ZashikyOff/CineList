6f8756ded5b9ebd127182c0c7e944c160d95dc29
ZashikyOff <119845760+ZashikyOff@users.noreply.github.com>
2023-03-17 15:53:35 +0100

    latest

--- core/header.php
+++ core/header.php
@@ -9,5 +9,5 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="CSS/main.css">
-        <script src="js/app.js" defer></script>
+        <script src="js/main.js" defer></script>
     </head>
--- list.php
+++ list.php
@@ -43,7 +43,7 @@ if ($queryfav->execute()) {
 
 function FindByIdMovie(int $id): array
 {
-    $sql = "SELECT titre FROM film WHERE id LIKE $id";
+    $sql = "SELECT titre FROM film WHERE id LIKE $id ORDER by titre";
     require "./core/config.php";
 
     $queryfilm = $lienDB->prepare($sql);
@@ -57,7 +57,7 @@ function FindByIdMovie(int $id): array
 }
 function FindByIdSerie(int $id): array
 {
-    $sql = "SELECT titre FROM serie WHERE id LIKE $id";
+    $sql = "SELECT titre FROM serie WHERE id LIKE $id ORDER BY titre";
     require "./core/config.php";
 
     $queryfilm = $lienDB->prepare($sql);
@@ -71,7 +71,7 @@ function FindByIdSerie(int $id): array
 }
 function FindByIdAnime(int $id): array
 {
-    $sql = "SELECT titre FROM anime WHERE id LIKE $id";
+    $sql = "SELECT titre FROM anime WHERE id LIKE $id ORDER BY titre";
     require "./core/config.php";
 
     $queryfilm = $lienDB->prepare($sql);
@@ -83,12 +83,39 @@ function FindByIdAnime(int $id): array
     }
     return $resultanimef;
 }
+// Ajouter Something --------------------------------
+if (isset($_POST["titre"]) && isset($_POST["genre"])) {
+    require "./core/config.php";
+
+    //Valeurs du formaulaire
+    //Email du compte
+    $titre = htmlspecialchars($_POST["titre"]);
+    //Password du compte
+    $genre = htmlspecialchars($_POST["genre"]);
+
+    $add = "INSERT INTO $genre (titre) VALUES (:titre)";
+    $query = $lienDB->prepare($add);
+
+    // Liaison des paramètres de la requête préparée
+    $query->bindParam(":titre", $titre);
+    // $query->bindParam(":genre", $genre);
+
+    try {
+        if ($query->execute()) {
+            echo "<p>Ajoutez</p>";
+        }
+    }
+    //catch exception
+    catch (Exception $e) {
+        echo 'Message: ' . $e->getMessage();
+    }
+}
 ?>
 
 <body>
     <script src="JS/main.js"></script>
     <script src="https://unpkg.com/htmx.org@1.8.5"></script>
-    <header class="d-flex flex-column justify-content-center align-items-center">
+    <header>
         <a class="nav-link active" aria-current="page" href="logout.php">Se deconnecter</a>
         <br>
         <h1>List of <?php echo $_SESSION["pseudo"] ?></h1>
@@ -103,68 +130,113 @@ function FindByIdAnime(int $id): array
     ?>
     </p>
     </header>
-    <main class="d-flex flex-column align-items-center">
-        <div class="film">
-            <h2>Films</h2>
-            <?php
-            $x = 0;
-            while ($x < (count($resultfilm))) {
-
-            ?><div class="article">
-                    <h5><?= $resultfilm[$x]["titre"] ?></h5>
-                </div><?php
-                        $x++;
-                    } ?>
+    <main class="liste">
+        <div class="add">
+            <h4>Add Something</h4>
+            <form action="" method="post">
+                <input type="text" name="titre" placeholder="Entrez le titre *" required>
+                <select name="genre" id="" required>
+                    <option value="">---- Choose ----</option>
+                    <option value="serie">Serie</option>
+                    <option value="film">Film</option>
+                    <option value="anime">Animé</option>
+                </select>
+                <button type="submit">-- Add ---</button>
+            </form>
         </div>
-        <div class="serie">
-            <h2>Serie</h2>
-            <?php
-            $x = 0;
-            while ($x < (count($resultserie))) {
-
-            ?><div class="article">
-                    <h5><?= $resultserie[$x]["titre"] ?></h5>
-                </div><?php
-                        $x++;
-                    } ?>
+        <div class="remove">
+            <h4>Remove Something</h4>
+            <form action="" method="post">
+                <select name="genre" id="">
+                    <option value="">---- Choose ----</option>
+                    <option value="serie">-- Serie --</option>
+                    <?php
+                    $x = 0;
+                    while ($x < (count($resultserie))) { ?>
+                        <option><?= $resultserie[$x]["titre"] ?></option><?php
+                                                                            $x++;
+                                                                        } ?>
+                    <option value="film">-- Film --</option>
+                    <?php
+                    $x = 0;
+                    while ($x < (count($resultfilm))) { ?>
+                        <option><?= $resultfilm[$x]["titre"] ?></option><?php
+                                                                        $x++;
+                                                                    } ?>
+                    <option value="anime">-- Animé --</option>
+                    <?php
+                    $x = 0;
+                    while ($x < (count($resultanime))) { ?>
+                        <option><?= $resultanime[$x]["titre"] ?></option><?php
+                                                                            $x++;
+                                                                        } ?>
+                </select>
+                <button type="button">-- Remove ---</button>
+            </form>
         </div>
-        <div class="anime">
-            <h2>Animé</h2>
-            <?php
-            $x = 0;
-            while ($x < (count($resultanime))) {
+        <div class="liste">
+            <div class="film">
+                <h2>Films</h2>
+                <?php
+                $x = 0;
+                while ($x < (count($resultfilm))) {
 
-            ?><div class="article">
-                    <h5><?= $resultanime[$x]["titre"] ?></h5>
-                </div><?php
-                        $x++;
-                    } ?>
-        </div>
-        <div class="favs">
-            <h2>Favories</h2>
-            <?php
-            $x = 0;
-            while ($x < (count($resultfav))) {
-            ?>
+                ?><div class="article">
+                        <h5><?= $resultfilm[$x]["titre"] ?></h5>
+                    </div><?php
+                            $x++;
+                        } ?>
+            </div>
+            <div class="serie">
+                <h2>Serie</h2>
                 <?php
-                if (strlen($resultfav[$x]["id_film"]) >= 1) {
-                ?><h5><?= FindByIdMovie($resultfav[$x]["id_film"])[$x]["titre"] ?? ""; ?></h5><?php
-                                                                                            }
-                                                                                                ?>
-                <?php
-                if (strlen($resultfav[$x]["id_serie"]) >= 1) {
-                ?><h5><?= FindByIdSerie($resultfav[$x]["id_serie"])[$x]["titre"] ?? ""; ?></h5><?php
-                                                                                            }
-                                                                                                ?>
-                <?php
-                if (strlen($resultfav[$x]["id_anime"]) >= 1) {
-                ?><h5><?= FindByIdAnime($resultfav[$x]["id_anime"])[$x]["titre"] ?? ""; ?></h5><?php
-                                                                                            }
-                                                                                                ?>
+                $x = 0;
+                while ($x < (count($resultserie))) {
 
-            <?php
-                $x++;
-            } ?>
+                ?><div class="article">
+                        <h5><?= $resultserie[$x]["titre"] ?></h5>
+                    </div><?php
+                            $x++;
+                        } ?>
+            </div>
+            <div class="anime">
+                <h2>Animé</h2>
+                <?php
+                $x = 0;
+                while ($x < (count($resultanime))) {
+
+                ?><div class="article">
+                        <h5><?= $resultanime[$x]["titre"] ?></h5>
+                    </div><?php
+                            $x++;
+                        } ?>
+            </div>
+            <div class="favs">
+                <h2>Favories</h2>
+                <?php
+                $x = 0;
+                while ($x < (count($resultfav))) {
+                ?>
+                    <?php
+                    if (strlen($resultfav[$x]["id_film"]) >= 1) {
+                    ?><h5><?= FindByIdMovie($resultfav[$x]["id_film"])[$x]["titre"] ?? ""; ?></h5><?php
+                                                                                                }
+                                                                                                    ?>
+                    <?php
+                    if (strlen($resultfav[$x]["id_serie"]) >= 1) {
+                    ?><h5><?= FindByIdSerie($resultfav[$x]["id_serie"])[$x]["titre"] ?? ""; ?></h5><?php
+                                                                                                }
+                                                                                                    ?>
+                    <?php
+                    if (strlen($resultfav[$x]["id_anime"]) >= 1) {
+                    ?><h5><?= FindByIdAnime($resultfav[$x]["id_anime"])[$x]["titre"] ?? ""; ?></h5><?php
+                                                                                                }
+                                                                                                    ?>
+
+                <?php
+                    $x++;
+                } ?>
+            </div>
         </div>
     </main>
     <footer>
